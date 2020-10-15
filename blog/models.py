4946df6bda399b904840e119183c3c69f99ead00
Hamid <abdelhamid.ouassini@hotmail.be>
2020-10-16 00:39:24 +0200

    Jobs app up and running

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+#Create blog Models
+
+"""
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+"""
+
+#add blog app to the settings
+
+#create migration
+
+#migrate
+
+#add admin
+
+
